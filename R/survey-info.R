9eef816cb1894d404efa95ae16d1b3a5f98c760a
Brandon Edwards <brandon.edwards144@gmail.com>
2022-01-19 15:44:14 -0500

    survey information function

---
+++ R/survey-info.R
@@ -0,0 +1,68 @@
+#' Get attributes for survey method
+#'
+#' \code{survey_info} returns a list of attributes associated with the survey method code.
+#'
+#' @param code Alphabetical code for survey method.
+#' @param model "rem" for removal model, "dis" for distance model
+#'
+#' @return List with the following entries:
+#'   \item{Method}{Alphabetical code for the survey code}
+#'   \item{Model}{"rem" or "dis" depending on user's entry}
+#'   \item{Description}{Description of the distance or time bins in this method}
+#'   \item{Max_Interval}{Maximum time or distance considered for this method}
+#'   \item{Units}{"Minutes" if removal model selected, "Metres" if distance model selected}
+#'
+#' @examples
+#'
+#'
+#' @export
+#'
+
+survey_info <- function(code = NULL,
+                        model = NULL)
+{
+  if (is.null(code))
+  {
+    stop("Survey method code not entered.")
+  }
+
+  if (is.null(model) || isFALSE(model %in% c("rem", "dis")))
+  {
+    stop("Enter \"dis\" or \"rem\" for the model.")
+  }
+
+  if (model == "rem")
+  {
+    survey <- utils::read.csv(system.file("survey",
+                                          "time_lookup.csv",
+                                          package = "napops"))
+  }
+  if (model == "dis")
+  {
+    survey <- utils::read.csv(system.file("survey",
+                                          "distance_lookup.csv",
+                                          package = "napops"))
+  }
+
+  code <- toupper(code)
+
+  if (isFALSE(toupper(code) %in% survey$Method))
+  {
+    stop("Invalid survey code. Use covariates_removal() or covariates_distance() to get valid survey codes.")
+  }
+
+  to_return <- list(Method = toupper(code),
+                    Model = model,
+                    Description = survey[survey$Method == code, "Description"][1],
+                    Max_Interval = survey[survey$Method == code, ncol(survey)][1])
+
+  if (model == "rem")
+  {
+    to_return$Units <- "Minutes"
+  }else
+  {
+    to_return$Units <- "Metres"
+  }
+
+  return(to_return)
+}
